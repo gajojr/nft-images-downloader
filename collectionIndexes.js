cff82a6dbb515551abbaa2c362a0750160362e8f
Andrija Gajic <andrijagajicbusiness@gmail.com>
2022-04-30 18:31:16 +0200

    fix: everything is now programatically executed and fix the problem with images having wrong names

--- collectionIndexes.js
+++ collectionIndexes.js
@@ -10,55 +10,84 @@ if (!url) {
 
 const collectionName = url.substring(url.lastIndexOf('/') + 1);
 
-var numberOfPages = 27;
-
 (async() => {
     try {
-        await fsPromises.access(`${collectionName}-collection-indexes.txt`)
         await fsPromises.rm(`${collectionName}-collection-indexes.txt`);
     } catch (err) {
-        console.log(err)
-        console.log('error occurred while deleting old file')
+        // error occurrs when file doesn't exist, since it's trying to access non existing file
     }
+
+    // in both cases new file will be created
+    console.log('creating file for storing collection data...');
 })();
 
-(async() => {
+var numberOfPages = 0;
+
+async function getNumberOfPages() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    let currentPageIndex = 0;
 
-    await page.goto(`${url}/?page=${currentPageIndex}&ids=&sort_by=rank`, { waitUntil: 'load', timeout: 0 });
-    await getCollectionIndexesFromCurrentPage();
+    await page.goto(url, { waitUntil: 'load', timeout: 0 });
 
-    async function getCollectionIndexesFromCurrentPage() {
-        const collectionIndexesElements = await page.$$('.item_stats>div:nth-child(2)>span');
+    const element = await page.waitForSelector('.pager>a:last-child');
+    numberOfPages = Number(await element.evaluate(el => el.textContent));
 
-        await Promise.all(
-            collectionIndexesElements.map(element => {
-                const collectionIndex = element.evaluate(el => el.textContent.substring(1))
-                collectionIndex.then(text => fsPromises.appendFile(`${collectionName}-collection-indexes.txt`, `${text}\n`));
-                return collectionIndex;
-            })
-        );
+    await browser.close();
 
-        currentPageIndex++;
+    console.log(`number of pages to scan: ${numberOfPages}`);
+}
 
-        if (currentPageIndex < numberOfPages) {
-            await switchPage(currentPageIndex);
-        } else {
-            await browser.close();
-        }
-    }
+getNumberOfPages()
+    .then(async() => {
+        console.log('launching the browser instance...');
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        let currentPageIndex = 0;
 
-    async function switchPage(currentPageIndex) {
-        await Promise.all([
-            page.click(`a[href='/${collectionName}/?page=${currentPageIndex}&ids=&sort_by=rank']`),
-            page.waitForNavigation({
-                waitUntil: 'load',
-                timeout: 0
-            }),
-        ]);
-
+        console.log('collecting data in progress, this might take long(up to 10 mins) depending on the size of the collections and internet speed...');
+        await page.goto(`${url}/?page=${currentPageIndex}&ids=&sort_by=rank`, { waitUntil: 'load', timeout: 0 });
         await getCollectionIndexesFromCurrentPage();
-    }
-})();
+
+        async function getCollectionIndexesFromCurrentPage() {
+            const collectionIndexesElements = await page.$$('.item_stats>div:nth-child(2)>span');
+            const imageElements = await page.$$('.featured_item_img>a>img');
+
+            await Promise.all(
+                collectionIndexesElements.map((element, idx) => {
+                    const collectionIndex = element.evaluate(el => el.textContent.substring(1));
+                    const imageUrl = imageElements[idx].evaluate(el => el.src);
+
+                    let contentToWrite = '';
+                    imageUrl
+                        .then(text => {
+                            contentToWrite += text
+                        })
+                        .then(() => {
+                            collectionIndex.then(text => contentToWrite += ` ${text}`)
+                        })
+                        .then(() => fsPromises.appendFile(`${collectionName}-collection-indexes.txt`, `${contentToWrite}\n`));
+                })
+            );
+
+            currentPageIndex++;
+
+            if (currentPageIndex < numberOfPages) {
+                await switchPage(currentPageIndex);
+            } else {
+                await browser.close();
+                console.log('collection data collected.');
+            }
+        }
+
+        async function switchPage(currentPageIndex) {
+            await Promise.all([
+                page.click(`a[href='/${collectionName}/?page=${currentPageIndex}&ids=&sort_by=rank']`),
+                page.waitForNavigation({
+                    waitUntil: 'load',
+                    timeout: 0
+                }),
+            ]);
+
+            await getCollectionIndexesFromCurrentPage();
+        }
+    });
--- index.js
+++ index.js
@@ -30,9 +30,10 @@ const collectionName = url.substring(url.lastIndexOf('/') + 1);
     }
 })();
 
-const collectionIndexes = [];
+const collectionIndexes = {};
 
 async function processCollectionIndexes() {
+    console.log('reading the text file containg the collection data...');
     const fileStream = fs.createReadStream(`${collectionName}-collection-indexes.txt`);
 
     const rl = readline.createInterface({
@@ -41,63 +42,65 @@ async function processCollectionIndexes() {
     });
 
     for await (const line of rl) {
-        collectionIndexes.push(line)
+        const [key, value] = line.split(' ');
+        collectionIndexes[key] = value;
     }
+
+    console.log('file read successfully');
 }
 
-var numberOfPages = 27;
+var numberOfPages = 0;
 
-// (async() => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-
-//     await page.goto(url);
-
-//     const element = await page.waitForSelector('.pager>a:last-child');
-//     numberOfPages = Number(await element.evaluate(el => el.textContent));
-//     console.log(numberOfPages)
-
-//     await browser.close();
-// })();
-
-(async() => {
-    await processCollectionIndexes();
+async function getNumberOfPages() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    let currentPageIndex = 0;
-    let numberOfImagesSavedPerPage = 0;
-    let totalImagesSaved = 0;
 
-    page.on('response', async response => {
-        const url = response.url();
-        if (response.request().resourceType() === 'image') {
-            response.buffer().then(async file => {
-                if (url.indexOf(`https://media.howrare.is/images/${collectionName}`) !== -1) {
-                    // console.log(collectionIndexes[totalImagesSaved]);
-                    // const fileName = url.split('/').pop();
-                    const filePath = path.resolve(__dirname, collectionName, `${collectionIndexes[totalImagesSaved]}.jpg`);
-                    const writeStream = fs.createWriteStream(filePath);
-                    writeStream.write(file);
-                    numberOfImagesSavedPerPage++;
-                    totalImagesSaved++;
-                    if (numberOfImagesSavedPerPage === 250) {
-                        if (currentPageIndex < numberOfPages - 1) {
-                            currentPageIndex++;
-                            numberOfImagesSavedPerPage = 0;
-                            switchPage(currentPageIndex);
+    await page.goto(url, { waitUntil: 'load', timeout: 0 });
+
+    const element = await page.waitForSelector('.pager>a:last-child');
+    numberOfPages = Number(await element.evaluate(el => el.textContent));
+
+    await browser.close();
+
+    console.log(`number of pages to scan: ${numberOfPages}`);
+}
+
+getNumberOfPages()
+    .then(() => processCollectionIndexes())
+    .then(async() => {
+        console.log('launching the browser instance...');
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        let currentPageIndex = 0;
+        let numberOfImagesSavedPerPage = 0;
+
+        page.on('response', response => {
+            const url = response.url();
+            if (response.request().resourceType() === 'image') {
+                response.buffer().then(file => {
+                    if (url.indexOf(`https://media.howrare.is/images/${collectionName}`) !== -1) {
+                        const filePath = path.resolve(__dirname, collectionName, `${collectionIndexes[url]}.jpg`);
+                        const writeStream = fs.createWriteStream(filePath);
+                        writeStream.write(file);
+                        numberOfImagesSavedPerPage++;
+                        if (numberOfImagesSavedPerPage === 250) {
+                            if (currentPageIndex < numberOfPages - 1) {
+                                currentPageIndex++;
+                                numberOfImagesSavedPerPage = 0;
+                                switchPage(currentPageIndex);
+                            }
                         }
                     }
-                }
-            });
+                });
+            }
+        });
+
+        await page.goto(`${url}/?page=${currentPageIndex}&ids=&sort_by=rank`, { waitUntil: 'load', timeout: 0 });
+
+        async function switchPage(currentPageIndex) {
+            await Promise.all([
+                page.click(`a[href='/${collectionName}/?page=${currentPageIndex}&ids=&sort_by=rank']`),
+                page.waitForNavigation({ waitUntil: 'load', timeout: 0 }),
+            ]);
         }
     });
-
-    await page.goto(`${url}/?page=${currentPageIndex}&ids=&sort_by=rank`, { waitUntil: 'load', timeout: 0 });
-
-    async function switchPage(currentPageIndex) {
-        await Promise.all([
-            page.click(`a[href='/${collectionName}/?page=${currentPageIndex}&ids=&sort_by=rank']`),
-            page.waitForNavigation({ waitUntil: 'load', timeout: 0 }),
-        ]);
-    }
-})();
